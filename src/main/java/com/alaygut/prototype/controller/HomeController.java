d6e14de188ea2dd3135c655d96ba1465f15dd3c8
Tuna <alaygut@gmail.com>
2019-07-03 15:50:04 +0300

    login error handle edildi.

--- src/main/java/com/alaygut/prototype/controller/HomeController.java
+++ src/main/java/com/alaygut/prototype/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.alaygut.prototype.controller;
 
-import org.springframework.stereotype.Controller; 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,6 +13,13 @@ public class HomeController {
 
     @GetMapping("/login")
     public String getLoginPage(){
+        return "login";
+    }
+
+    // Login form with error
+    @GetMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("error", true);
         return "login";
     }
 
