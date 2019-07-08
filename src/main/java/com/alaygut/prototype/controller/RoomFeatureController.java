4e2cf1608b5abbd84afa159eb518b95c1fd5ef13
Dilara Gebes <dilara.gebes@hotmail.com>
2019-07-08 11:36:42 +0300

    appppp

--- src/main/java/com/alaygut/prototype/controller/RoomFeatureController.java
+++ src/main/java/com/alaygut/prototype/controller/RoomFeatureController.java
@@ -71,7 +71,7 @@ public class RoomFeatureController {
 		RoomFeature roomFeature = roomFeatureService.getRoomFeature(idTransfer.getRecordId());
 		addRoomFeatureForm.setFeatureName(roomFeature.getFeatureName());
 		addRoomFeatureForm.setDescription(roomFeature.getDescription());
-		addRoomFeatureForm.setRecordId(roomFeature.getFeatureId());
+		addRoomFeatureForm.setRecordId(roomFeature.getRoomFeatureId());
 		
 		
 		return new ModelAndView("editRoomFeature", "addRoomFeatureForm", addRoomFeatureForm);
